655b3fd118a96234b93af1dd44a7a1f846384dc1
jhon fredy <joberdigital6@gmail.com>
2024-09-29 00:12:35 -0500

    using random obtain of input how much numbers for output from step 1

--- assets/script.js
+++ assets/script.js
@@ -4,32 +4,23 @@ let contrasena = document.getElementById("contrasena");
 
 const cadenaCaracteres =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-
-
-
-  const elegirPassword = () => {
-    let numeroDigitado = parseInt(cantidad.value);
-    console.log(numeroDigitado);
-    if (numeroDigitado < 1) {
-        alert("La cantidad de caracteres tienen que ser mayor a 0");
-       }    
-        generar(numeroDigitado);
-        cantidad.value=null
-     
-    }    
-
   
+const elegirPassword = () => {
+  let numeroDigitado = parseInt(cantidad.value);
+  console.log(numeroDigitado);
+  if (numeroDigitado < 1) {
+  alert("La cantidad de caracteres tienen que ser mayor a 0");
+}    
+    generar(numeroDigitado);
+    cantidad.value=null
+}    
 function generar(numeroDigitado) {
     let password = "";
-for (let i = 0; i < numeroDigitado; i++) {
+    for (let i = 0; i < numeroDigitado; i++) {
     let caracterAleatorio =
-   cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
-   console.log(caracterAleatorio);
-   password += caracterAleatorio;
+    cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
+    password += caracterAleatorio;
 }
    contrasena.value = password;
+   console.log(caracterAleatorio);
 }
-
-
-
